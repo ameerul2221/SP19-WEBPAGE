51bd7b61fa11066468a6cfdb1685af4b2a70e310
ameerul2221 <ameerulhaq11@gmail.com>
2024-03-28 22:13:34 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("Welcome to the CovidBAT:BookAndTrial! We will send you the latest information of COVID-19! And newsletter on subscribing to our website on booking and registering
+ your appointment from any covid-19 infections! Thanks!!");
